c33db3235a97d9c3176274b789f9fe41a1fa106e
Jun <cmoonjun11@gmail.com>
2023-05-21 03:43:51 +0900

    ResultPage, 확인사항 컴포넌트

--- front/pages/components/Result/Report.js
+++ front/pages/components/Result/Report.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import TopPriorityRightToCompensation from "./CheckTab/TopPriorityRightToCompensation";
+import CheckList from "./CheckTab/CheckList";
 
 const Report = ({ result }) => {
   let parsedResult = {};
@@ -7,9 +10,34 @@ const Report = ({ result }) => {
     parsedResult = JSON.parse(result) || [];
   }
 
-  console.log(parsedResult.address);
+  const Separator = () => {
+    return (
+      <hr
+        style={{
+          border: "1px solid gray",
+          marginTop: "40px",
+          marginBottom: "20px",
+        }}
+      />
+    );
+  };
 
-  return <>{result}</>;
+  return (
+    <>
+      {result}
+      <Container>
+        <div>
+          <h2 style={{ paddingTop: "20px", marginRight: "10px" }}>확인사항</h2>
+        </div>
+        <TopPriorityRightToCompensation
+          parsedResult={parsedResult}
+        ></TopPriorityRightToCompensation>
+        <Separator />
+
+        <CheckList parsedResult={parsedResult}></CheckList>
+      </Container>
+    </>
+  );
 };
 
 export default Report;
