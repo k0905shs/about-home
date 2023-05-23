66f4ce08b79b5a8c514b4556d9761079489a1059
Jun <cmoonjun11@gmail.com>
2023-05-23 17:55:39 +0900

    공지사항 준비

--- pages/components/BulletinBoard/BoardList.js
+++ pages/components/BulletinBoard/BoardList.js
@@ -4,25 +4,25 @@ import { Table, Container } from "react-bootstrap";
 const BoardList = () => {
   return (
     <>
-      <h2 style={{ marginTop: "20px" }}>공지사항</h2>
+      <h2 style={{ marginTop: "20px", marginBottom: "20px" }}>공지사항</h2>
       <Container>
         <Table striped bordered hover style={{ textAlign: "center" }}>
           <thead>
             <tr>
               <th style={{ width: "10%" }}>번호</th>
-              <th style={{ width: "60%" }}>제목</th>
+              <th style={{ width: "50%" }}>제목</th>
               <th style={{ width: "10%" }}>작성자</th>
-              <th style={{ width: "10%" }}>날짜</th>
+              <th style={{ width: "15%" }}>날짜</th>
               <th style={{ width: "10%" }}>조회수</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>1</td>
-              <td>Mark</td>
-              <td>dsadsadsa</td>
-              <td>dsadas</td>
-              <td>@mdo</td>
+              <td>아직 준비가 안됐어요!!</td>
+              <td>Kim</td>
+              <td>2023-05-23</td>
+              <td>999</td>
             </tr>
           </tbody>
         </Table>
